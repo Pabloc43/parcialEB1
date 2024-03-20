36d76e269f3253413fe2b53696bcfef886e69e29
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2024-03-19 23:40:28 -0300

    Including Load Balancer Random

--- catalog-service/src/main/java/com/dh/catalogservice/Client/IMovie.java
+++ catalog-service/src/main/java/com/dh/catalogservice/Client/IMovie.java
@@ -2,6 +2,8 @@ package com.dh.catalogservice.Client;
 
 
 import com.dh.catalogservice.Model.DTO.Movie;
+import com.dh.catalogservice.configuration.CustomLoadBalancerConfiguration;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 @FeignClient("movie-service")
+@LoadBalancerClient(name = "movie-service", configuration = CustomLoadBalancerConfiguration.class)
 public interface IMovie {
         @GetMapping("/api/v1/movies/{genre}")
         ResponseEntity<List<Movie>> getMovieByGenre(@PathVariable String genre);
